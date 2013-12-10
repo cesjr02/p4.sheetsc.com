4a544a8025c182cae59d5a9ece47a1e103911dc7
CJ Sheets <sheets@g.harvard.edu>
2013-12-09 20:45:40 -0600

    nav and basic layout/ link to external style and scripts

--- views/_v_template.php
+++ views/_v_template.php
@@ -4,32 +4,104 @@
 	<title><?php if(isset($title)) echo $title; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<!-- bootstrap -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
 	
+	<!-- Bootstrap -->
+	<link href='/css/bootstrap.min.css' rel='stylesheet'>
+	
+	<!-- style.css -->
+	<link href='/css/style.css' rel='stylesheet'>
+	
+	<!-- rate it (star rating) -->
+	<link href='/css/rateit.css' rel='stylesheet'>
+
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->	
-					
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>
+	<script src='https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js'></script>
+	<![endif]-->
+
+
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
-<body>	
+<body>
+	<!-- fixed navbar -->
+	<nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
+		<div class='container'>
+			<div class='navbar-header'>
+			
+				<!-- button displays when on mobile devices -->
+				<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
+					<span class="sr-only">Toggle navigation</span>
+					<span class='icon-bar'></span>
+					<span class='icon-bar'></span>
+					<span class='icon-bar'></span>
+				</button>
+				
+				<a href='#' class='navbar-brand'>Bloody Mary Blog</a>
+			</div>
+			
+			<div class='collapse navbar-collapse'>
+				<ul class='nav navbar-nav navbar-right'>
+				
+					<!-- Menu for logged in users -->
+					<?php if($user): ?>
+					<li class='dropdown'>
+					
+						<a href='#' class='dropdown-toggle' data-toggle='dropdown'><?php echo $user->first_name; ?> <?php echo $user->last_name; ?> <b class='caret'></b></a>
+						
+						<ul class='dropdown-menu'>
+							<li><a href='/users/profile'><span class='glyphicon glyphicon-user'></span> View My Profile</a></li>
+							<li class='divider'></li>
+							<li><a href='/reviews/add'><span class='glyphicon glyphicon-comment'></span> Post</a></li>
+							<li class='divider'></li>
+							<li><a href='/reviews'><span class='glyphicon glyphicon-th-list'></span> Reviews</a></li>
+							<li class='divider'></li>
+							<li><a href='/reviews/users'><span class='glyphicon glyphicon-link'></span> Connect</a></li>
+					
+						</ul>
+					</li>
+						
+					<li><a href='/users/logout/'>Sign out</a></li>
+						
+					<?php else: ?>
+					<!-- Menu for users who are not logged in -->
+					
+					<li><a href='/'>Home</a></li>
+					<li><a href='/users/signup'>Register</a></li>					
+					<?php endif; ?>				
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<div class='container'>
+		<div class='row'>
+			<?php if(isset($content)) echo $content; ?>
+		</div><!-- end of row -->
+	</div><!-- end of container -->
 
-	<?php if(isset($content)) echo $content; ?>
 
-	<?php if(isset($client_files_body)) echo $client_files_body; ?>
+	<!-- footer -->
+	<div class='navbar navbar-default navbar-fixed-bottom'>
+		<div class='container'>
+			<p class='navbar-text pull-right'>Bloody Mary Blog - Developed by CJ Sheets</p>
+		</div>	
+	</div>
+	
 	
 	
 	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+	<script src='https://code.jquery.com/jquery.js'></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src='/js/bootstrap.min.js'></script>
+	<!-- rate it js -->
+	<script src='/js/jquery.rateit.min.js'></script>
+	
+	<?php if(isset($client_files_body)) echo $client_files_body; ?>
 </body>
 </html>
