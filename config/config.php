99d5b4246f52da062a6e2d16c9b0a08b45b5019e
CJ Sheets <sheets@g.harvard.edu>
2013-12-09 20:44:20 -0600

    add config settings

--- config/config.php
+++ config/config.php
@@ -14,11 +14,11 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Sample App');  
+	define('APP_NAME', 'Bloody Mary Blog');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
-	define('APP_EMAIL', 'webmaster@sample-app.com'); 
+	define('APP_EMAIL', 'cj@sheetsc.com'); 
 
 /* 
 A email designated to receive messages from the server. Examples:
@@ -28,13 +28,13 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'cj@sheetsc.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'myapp_com'); 
+	define('DB_NAME', 'sheetscc_p4_sheetsc_com'); 
 
 # Timezone
-	define('TIMEZONE', 'America/New_York');
+	define('TIMEZONE', 'America/Chicago');
 
 # If your app is going to have outgoing emails, you should fill in your SMTP settings
 # For this you could use gmail SMTP or something like http://sendgrid.com/
@@ -43,5 +43,5 @@ A email designated to receive messages from the server. Examples:
 	//define('SMTP_PASSWORD', '');
 
 # For extra security, you might want to set different salts than what the core uses
-	//define('PASSWORD_SALT', '');
-	//define('TOKEN_SALT', '');
+	define('PASSWORD_SALT', 'g@Y)(wn0bvqXXtW_!aP_m8PQk3W4X0HvO)7Hux%8');
+	define('TOKEN_SALT', 'Mu8c7-9G)MJh(&0k4kJQubJnxt#6U3%&^*7t9-QC');
