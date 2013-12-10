7d0f429f2f7d7fa5d920e47e3bc116416ec14ac9
CJ Sheets <sheets@g.harvard.edu>
2013-12-09 20:44:27 -0600

    clean up

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -1,39 +1,43 @@
 <?php
 
 class index_controller extends base_controller {
-	
-	/*-------------------------------------------------------------------------------------------------
 
-	-------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------*/
+
 	public function __construct() {
 		parent::__construct();
 	} 
-		
-	/*-------------------------------------------------------------------------------------------------
-	Accessed via http://localhost/index/index/
-	-------------------------------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------------------
+	 accessed via http://localhost/index/index/
+-------------------------------------------------------------------------------------------------*/
+
 	public function index() {
 		
-		# Any method that loads a view will commonly start with this
-		# First, set the content of the template with a view file
-			$this->template->content = View::instance('v_index_index');
-			
-		# Now set the <title> tag
-			$this->template->title = "Hello World";
+		// any method that loads a view will commonly start with this
+		// first, set the content of the template with a view file
+		$this->template->content = View::instance('v_index_index');
+		$this->template->content->loginContent = View::instance('v_users_login');
+		// $this->template->content->signupContent = View::instance('v_users_signup');
+
+		// now set the <title> tag
+		$this->template->title = "Bloody Mary Blog";
 	
-		# CSS/JS includes
+		// CSS/JS includes
 			/*
 			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
-	    	
-	    	$client_files_body = Array("");
-	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
-	    	*/
-	      					     		
-		# Render the view
-			echo $this->template;
+			$this->template->client_files_head = Utils::load_client_files($client_files);
+			
+			$client_files_body = Array("");
+			$this->template->client_files_body = Utils::load_client_files($client_files_body);
+			*/
 
-	} # End of method
-	
-	
-} # End of class
+		// render the view
+		echo $this->template;
+
+	} 
+
+
+} // eoc
