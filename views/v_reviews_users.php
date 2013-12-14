71a5b23060e9ee39608a83826aea29ae13844cfa
CJ Sheets <sheets@g.harvard.edu>
2013-12-14 15:09:37 -0600

    naming convention change

--- views/v_reviews_users.php
+++ views/v_reviews_users.php
@@ -4,21 +4,21 @@
 	
 	<p>Choose to follow or unfollow from the list of contributors below.</p>
 	
-	<? foreach($users as $yapper): ?>
+	<? foreach($users as $contributor): ?>
 	
 		<!-- you can't unfollow yourself so your button isn't shown -->
-		<?php if ($user->user_id != $yapper['user_id']) : ?>
+		<?php if ($user->user_id != $contributor['user_id']) : ?>
 	
 			<!-- print this user's name -->
-			<h3 class='h3'><?=$yapper['first_name']?> <?=$yapper['last_name']?></h3>
+			<h3 class='h3'><?=$contributor['first_name']?> <?=$contributor['last_name']?></h3>
 			
 			<!-- if there exists a connection with this user, show a unfollow link -->
-			<? if(isset($connections[$yapper['user_id']])): ?>
-				<a href='/posts/unfollow/<?=$yapper['user_id']?>'><input type='submit' value='Unfollow' class='btn btn-primary' ></a>
+			<? if(isset($connections[$contributor['user_id']])): ?>
+				<a href='/reviews/unfollow/<?=$contributor['user_id']?>'><input type='submit' value='Unfollow' class='btn btn-primary' ></a>
 				
 			<!-- otherwise, show the follow link -->
 			<? else: ?>
-				<a href='/posts/follow/<?=$yapper['user_id']?>'><input type='submit' value='Follow' class='btn btn-default' ></a>
+				<a href='/reviews/follow/<?=$contributor['user_id']?>'><input type='submit' value='Follow' class='btn btn-default' ></a>
 			<? endif; ?>
 			<br>
 		
