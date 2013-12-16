a9f20abd5456c1aa1e850ab3cbac08387d30117b
CJ Sheets <sheets@g.harvard.edu>
2013-12-15 21:17:43 -0600

    hide login form in user is logged in

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,6 +1,9 @@
 <div class='col-md-8'>
-	<h2 class='h2'>Bloody Mary Blog</h2>
-	<?=$loginContent;?>						
+	<h1 class='h1'>Bloody Mary Blog</h1>
+	<?php if($user): ?><h2 class='h2'>Hello, <?php echo $user->first_name; ?></h2>
+	<p class='p'>Click <?php echo "<a href='/users/profile/'>here</a>"; ?> to go to your profile.</p>
+	<?php else: ?><?=$loginContent;?>
+	<?php endif; ?>					
 </div>
 
 <div class='col-md-4'>
