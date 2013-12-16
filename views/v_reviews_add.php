1047a9415e6d40fe8b28d950af5dd81b97df8cd7
CJ Sheets <sheets@g.harvard.edu>
2013-12-16 14:10:09 -0600

    jQuery form validation add

--- views/v_reviews_add.php
+++ views/v_reviews_add.php
@@ -1,73 +1,81 @@
 <div class='col-md-6 col-md-offset-3'>
 	<!-- form for adding a new post -->
-	<form method='POST' action='/reviews/p_add'>
+	<form role="form" id="myForm" method='POST' action='/reviews/p_add'>
 		
 		<h2 class='h2'>Post your Bloody Mary Review</h2>
-		<select  class='form-control' name="state" id="state"> 
-			<option value="" selected="selected">Select a State</option> 
-			<option value="AL">Alabama</option> 
-			<option value="AK">Alaska</option> 
-			<option value="AZ">Arizona</option> 
-			<option value="AR">Arkansas</option> 
-			<option value="CA">California</option> 
-			<option value="CO">Colorado</option> 
-			<option value="CT">Connecticut</option> 
-			<option value="DE">Delaware</option> 
-			<option value="DC">District Of Columbia</option> 
-			<option value="FL">Florida</option> 
-			<option value="GA">Georgia</option> 
-			<option value="HI">Hawaii</option> 
-			<option value="ID">Idaho</option> 
-			<option value="IL">Illinois</option> 
-			<option value="IN">Indiana</option> 
-			<option value="IA">Iowa</option> 
-			<option value="KS">Kansas</option> 
-			<option value="KY">Kentucky</option> 
-			<option value="LA">Louisiana</option> 
-			<option value="ME">Maine</option> 
-			<option value="MD">Maryland</option> 
-			<option value="MA">Massachusetts</option> 
-			<option value="MI">Michigan</option> 
-			<option value="MN">Minnesota</option> 
-			<option value="MS">Mississippi</option> 
-			<option value="MO">Missouri</option> 
-			<option value="MT">Montana</option> 
-			<option value="NE">Nebraska</option> 
-			<option value="NV">Nevada</option> 
-			<option value="NH">New Hampshire</option> 
-			<option value="NJ">New Jersey</option> 
-			<option value="NM">New Mexico</option> 
-			<option value="NY">New York</option> 
-			<option value="NC">North Carolina</option> 
-			<option value="ND">North Dakota</option> 
-			<option value="OH">Ohio</option> 
-			<option value="OK">Oklahoma</option> 
-			<option value="OR">Oregon</option> 
-			<option value="PA">Pennsylvania</option> 
-			<option value="RI">Rhode Island</option> 
-			<option value="SC">South Carolina</option> 
-			<option value="SD">South Dakota</option> 
-			<option value="TN">Tennessee</option> 
-			<option value="TX">Texas</option> 
-			<option value="UT">Utah</option> 
-			<option value="VT">Vermont</option> 
-			<option value="VA">Virginia</option> 
-			<option value="WA">Washington</option> 
-			<option value="WV">West Virginia</option> 
-			<option value="WI">Wisconsin</option> 
-			<option value="WY">Wyoming</option>
-		</select>
-		<br>
+		<p><span class="label label-warning">All fields required</span></p>
+
+		<div class='form-group'>
+			<select  class='form-control' name="state" id="state"> 
+				<option value="" selected="selected">Select a State</option> 
+				<option value="AL">Alabama</option> 
+				<option value="AK">Alaska</option> 
+				<option value="AZ">Arizona</option> 
+				<option value="AR">Arkansas</option> 
+				<option value="CA">California</option> 
+				<option value="CO">Colorado</option> 
+				<option value="CT">Connecticut</option> 
+				<option value="DE">Delaware</option> 
+				<option value="DC">District Of Columbia</option> 
+				<option value="FL">Florida</option> 
+				<option value="GA">Georgia</option> 
+				<option value="HI">Hawaii</option> 
+				<option value="ID">Idaho</option> 
+				<option value="IL">Illinois</option> 
+				<option value="IN">Indiana</option> 
+				<option value="IA">Iowa</option> 
+				<option value="KS">Kansas</option> 
+				<option value="KY">Kentucky</option> 
+				<option value="LA">Louisiana</option> 
+				<option value="ME">Maine</option> 
+				<option value="MD">Maryland</option> 
+				<option value="MA">Massachusetts</option> 
+				<option value="MI">Michigan</option> 
+				<option value="MN">Minnesota</option> 
+				<option value="MS">Mississippi</option> 
+				<option value="MO">Missouri</option> 
+				<option value="MT">Montana</option> 
+				<option value="NE">Nebraska</option> 
+				<option value="NV">Nevada</option> 
+				<option value="NH">New Hampshire</option> 
+				<option value="NJ">New Jersey</option> 
+				<option value="NM">New Mexico</option> 
+				<option value="NY">New York</option> 
+				<option value="NC">North Carolina</option> 
+				<option value="ND">North Dakota</option> 
+				<option value="OH">Ohio</option> 
+				<option value="OK">Oklahoma</option> 
+				<option value="OR">Oregon</option> 
+				<option value="PA">Pennsylvania</option> 
+				<option value="RI">Rhode Island</option> 
+				<option value="SC">South Carolina</option> 
+				<option value="SD">South Dakota</option> 
+				<option value="TN">Tennessee</option> 
+				<option value="TX">Texas</option> 
+				<option value="UT">Utah</option> 
+				<option value="VT">Vermont</option> 
+				<option value="VA">Virginia</option> 
+				<option value="WA">Washington</option> 
+				<option value="WV">West Virginia</option> 
+				<option value="WI">Wisconsin</option> 
+				<option value="WY">Wyoming</option>
+			</select>
+		</div>
 		
-		<input type="text" name="city" id="city" class="form-control" placeholder="Enter city" required>
-		<br>
+		<div class='form-group'>
+			<input type="text" name="city" id="city" class="form-control" placeholder="Enter city" data-validation='length' data-validation-length='3-17' data-validation-error-msg='City must have (3-17 characters)' data-validation-help='Enter city (3-17 characters)'>
+		</div>
 		
-		<input type="text" name="location" id="location" class="form-control" placeholder="Enter Bar or Restaurant name" required>
-		<br>
-		<!-- maxlength defined as 255 -->
-		<textarea maxlength='255' name='review' id='review' class='form-control' rows='3'  placeholder="Write review" required></textarea>
-		<br>
+		<div class='form-group'>
+			<input type="text" name="location" id="location" class="form-control" placeholder="Enter Bar or Restaurant name" data-validation='length' data-validation-length='3-17' data-validation-error-msg='Your location must have (3-17 characters)' data-validation-help='Enter your location (3-17 characters)'>
+		</div>
 		
+		<div class='form-group'>
+			<!-- maxlength defined as 255 -->
+			<textarea maxlength='255' name='review' id='review' class='form-control' rows='3'  placeholder="Write review" data-validation='length' data-validation-length='50-255' data-validation-error-msg='Review must be (50-255 characters)' data-validation-help='Write a short review (50-255 characters)'></textarea>
+		</div>
+
+		<!-- star rating -->
 		<select id="backing2b" name="rating">
 			<option value="0">0</option>
 			<option value="1">1</option>
--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,37 +1,44 @@
 <div class='col-md-6 col-md-offset-3'>
 	<h2 class='h2'>Create Account</h2>
 	
-	<p class='error'>* required field.</p>
-						<!-- form for signup -->
-						<form method='POST' action='/users/p_signup'>
+	<p><span class="label label-warning">All fields required</span></p>
+		<!-- form for signup -->
+		<form method='POST' action='/users/p_signup'>
 		
-							First Name<span class='error'>*</span><br>
-							<input type='text' name='first_name' class='form-control input-lg' value='<?php if(isset($_POST['first_name'])) echo $_POST['first_name']?>'><br>
-					
-							Last Name<span class='error'>*</span><br>
-							<input type='text' name='last_name' class='form-control input-lg' value='<?php if(isset($_POST['last_name'])) echo $_POST['last_name']?>'><br>
-					
-							Email<span class='error'>*</span><br>
-							<input type='text' name='email' class='form-control input-lg' value='<?php if(isset($_POST['email'])) echo $_POST['email']?>'><br>
-						
-								
-							Password<span class='error'>*</span><br>
-							<input type='password' name='password' class='form-control input-lg'>
-							<br>
-							
-							Retype Password<span class='error'>*</span><br>
-							<input type='password' name='retype' class='form-control input-lg'>
-							<br>
-					
-							<!-- checks to see if error isset. If so, echo specific error. -->		
-							<?php if(isset($error)): ?>
-								<div class='alert alert-danger'>
-									Sign up failed.<br>
-									<?php echo $error; ?>
-								</div>
-							<?php endif; ?>  
-								
-							<input type='submit' class='btn btn-primary' value='Submit' >
+			<div class='form-group'>
+				<label class="sr-only" for="first_name">First Name</label>
+			    <input type='text' name='first_name' id="first_name" placeholder="Enter first name" class='form-control' data-validation="required" data-validation-error-msg="You must enter your first name" data-validation-help='Enter your first name' value='<?php if(isset($_POST['first_name'])) echo $_POST['first_name']?>'>
+			</div>
+		
+		    <div class='form-group'>
+		    	<label class="sr-only" for="last_name">Last Name</label>
+			    <input type='text' name='last_name' id="last_name" placeholder="Enter last name" class='form-control' data-validation="required" data-validation-error-msg="You must enter your last name" data-validation-help='Enter your last name' value='<?php if(isset($_POST['last_name'])) echo $_POST['last_name']?>'>
+		    </div>
+		
+		    <div class='form-group'>
+		    	<label class="sr-only" for="email">Email address</label>
+			    <input type='text' name='email' id="email" placeholder="Enter email" id="email" class='form-control' data-validation="email" value='<?php if(isset($_POST['email'])) echo $_POST['email']?>'>
+		    </div>
 
-						</form>
+		    <div class='form-group'>
+		    	<label class="sr-only" for="password">Password</label>
+		    	<input type='password' name='password' id="password" placeholder="Enter password" class='form-control' data-validation='length' data-validation-length='6-25' data-validation-error-msg='Password must have between 6-25 characters' data-validation-help='Enter password (6-25 characters)'>
+		    </div>
+		 
+		    <div class='form-group'>
+		    	<label class="sr-only" for="retype">Retype password</label>
+			    <input type='password' name='retype' id="retype" placeholder="Retype password" class='form-control'>
+		    </div>
+		
+		    <!-- checks to see if error isset. If so, echo specific error. -->		
+		    <?php if(isset($error)): ?>
+		    	<div class='alert alert-danger'>
+		    		Sign up failed.<br>
+		    		<?php echo $error; ?>
+		    	</div>
+		    <?php endif; ?>  
+		    	
+		    <input type='submit' class='btn btn-primary' value='Submit' >
+
+		</form>
 </div><!-- end of col -->
