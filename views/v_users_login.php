a19720d4104989e7ffa7bd12aeed6b90c05bfa01
CJ Sheets <sheets@g.harvard.edu>
2013-12-15 21:20:28 -0600

    column div added

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,11 +1,21 @@
-<form class="form-inline" method='POST' action='/users/p_login'>
-	<div class="form-group">
-		<label class="sr-only" for="loginemail">Email address</label>
-		<input type="email" name="email" class="form-control" id="loginemail" placeholder="Enter email" required>
-	</div>
-	<div class="form-group">
-		<label class="sr-only" for="loginpassword">Password</label>
-		<input type="password" name="password" class="form-control" id="loginpassword" placeholder="Password" required>
-	</div>
-	<button type="submit" class="btn btn-default">Sign in</button>
-</form>
+<div class='col-md-8'>
+	<form class="form-inline" method='POST' action='/users/p_login'>
+		<div class="form-group">
+			<label class="sr-only" for="loginemail">Email address</label>
+			<input type="email" name="email" class="form-control" id="loginemail" placeholder="Enter email" required>
+		</div>
+		<div class="form-group">
+			<label class="sr-only" for="loginpassword">Password</label>
+			<input type="password" name="password" class="form-control" id="loginpassword" placeholder="Password" required>
+		</div>
+		<button type="submit" class="btn btn-default">Sign in</button>
+		<br><br>
+		<!-- shows error if login failed -->		
+		<?php if(isset($error)): ?>
+			<div class='alert alert-danger'>
+				Oh no, signin failed. Please double check your email and password.
+			</div>
+			<br>
+		<?php endif; ?>
+	</form>
+</div> <!-- end of col -->
